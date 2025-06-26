3b9e1c005d486c6ed3d42759067bbf2ab1fbf9a5
Nodirbek Ergashev <nodirbekergashev1304@gmail.com>
2025-06-26 21:32:35 +0500

    I was add fields and imleement methods which classes given to me

--- src/main/java/uz/pdp/itemClasses/OrderItem.java
+++ src/main/java/uz/pdp/itemClasses/OrderItem.java
@@ -1,5 +1,10 @@
 package uz.pdp.itemClasses;
 
-public class OrderItem {
+import uz.pdp.enums.OrderStatus;
 
+import java.util.UUID;
+
+public class OrderItem {
+    private OrderStatus status;
+    private UUID cartId;
 }
