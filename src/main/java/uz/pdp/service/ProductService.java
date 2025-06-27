24123f75a22e141014afc08a77f53cc47c12f551
Ozodbek Umrzoqov <umrzoqovozodbek54@gmail.com>
2025-06-27 15:19:55 +0500

    ProductService class with all CRUD and filter methods implemented

--- src/main/java/uz/pdp/service/ProductService.java
+++ src/main/java/uz/pdp/service/ProductService.java
@@ -6,6 +6,7 @@ import uz.pdp.model.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ProductService implements BaseService<Product> {
     private List<Product> products = new ArrayList<>();
@@ -20,26 +21,40 @@ public class ProductService implements BaseService<Product> {
 
     @Override
     public void update(UUID id, Product product) {
-        for (Product old : products) {
-            if (old.getId().equals(id)) {
-
-            }
+        Product old = getById(id);
+        if (old != null) {
+            old.setName(product.getName());
+            old.setPrice(product.getPrice());
+            old.setSellerId(product.getSellerId());
+            old.setQuantity(product.getQuantity());
+            old.setUpdatedAt(product.getUpdatedAt());
+            old.setActive(product.isActive());
+            old.setCatId(product.getCatId());
+            old.setCreatedAt(product.getCreatedAt());
         }
     }
 
     @Override
     public boolean delete(UUID id) {
+        Product deletingProduct = getById(id);
+        if (deletingProduct != null) {
+            deletingProduct.setActive(false);
+            return true;
+        }
         return false;
     }
 
     @Override
     public Product getById(UUID id) {
-        return null;
+        return products.stream()
+                .filter(product -> product.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public List<Product> getAll() {
-        return List.of();
+        return products;
     }
 
     @Override
@@ -49,19 +64,29 @@ public class ProductService implements BaseService<Product> {
 
     @Override
     public String getCreatedTimeById() {
-
         return BaseService.super.getCreatedTimeById();
     }
 
     @Override
     public String getUpdatedTimeById() {
-
         return BaseService.super.getUpdatedTimeById();
+    }
+
+    public List<Product> getByCategory(String categoryName) {
+        return products.stream()
+                .filter(product -> product.getName().equals(categoryName) && product.isActive())
+                .collect(Collectors.toList());
     }
 
     public boolean isDefined(UUID sellerId, String productName) {
         return products.stream()
                 .anyMatch(product -> product.getSellerId().equals(sellerId) &&
                 product.getName().equalsIgnoreCase(productName));
+    }
+
+    public List<Product> getProductBySellerId(UUID sellerId) {
+        return products.stream()
+                .filter(product -> product.getSellerId().equals(sellerId))
+                .collect(Collectors.toList());
     }
 }
