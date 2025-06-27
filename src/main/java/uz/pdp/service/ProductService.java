3bde3289278647222be3f0d9b18e8247c5774d2d
Ozodbek Umrzoqov <umrzoqovozodbek54@gmail.com>
2025-06-27 11:46:06 +0500

    test

--- src/main/java/uz/pdp/service/ProductService.java
+++ src/main/java/uz/pdp/service/ProductService.java
@@ -3,18 +3,28 @@ package uz.pdp.service;
 import uz.pdp.baseAbs.BaseService;
 import uz.pdp.model.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class ProductService implements BaseService<Product> {
+    private List<Product> products = new ArrayList<>();
     @Override
     public boolean add(Product product) {
+        if (!isDefined(product.getSellerId(), product.getName())) {
+            products.add(product);
+            return true;
+        }
         return false;
     }
 
     @Override
     public void update(UUID id, Product product) {
+        for (Product old : products) {
+            if (old.getId().equals(id)) {
 
+            }
+        }
     }
 
     @Override
@@ -39,11 +49,19 @@ public class ProductService implements BaseService<Product> {
 
     @Override
     public String getCreatedTimeById() {
+
         return BaseService.super.getCreatedTimeById();
     }
 
     @Override
     public String getUpdatedTimeById() {
+
         return BaseService.super.getUpdatedTimeById();
+    }
+
+    public boolean isDefined(UUID sellerId, String productName) {
+        return products.stream()
+                .anyMatch(product -> product.getSellerId().equals(sellerId) &&
+                product.getName().equalsIgnoreCase(productName));
     }
 }
